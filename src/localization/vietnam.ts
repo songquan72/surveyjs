4625ac78ee0922cb4ccd4cb99803e036adef2feb
songquan72 <songquancnttbk@gmail.com>
2017-03-16 17:35:56 +0700

    Create vietnam.ts

---
+++ src/localization/vietnam.ts
@@ -0,0 +1,34 @@
+
+import {surveyLocalization} from "../surveyStrings";
+var vietnamSurveyStrings = {
+		    pagePrevText: "Trước",
+		    pageNextText: "Sau",
+		    completeText: "Hoàn Thành",
+		    otherItemText: "Khác (Mô tả)",
+		    progressText: "Trang {0} of {1}",
+		    emptySurvey: "Không có trang hoặc câu hỏi hiển thị trong cuộc khảo sát.",
+		    completingSurvey: "Cảm ơn bạn đã hoàn thành bài khảo sát này!",
+		    loadingSurvey: "Khảo sát đang tải...",
+		    optionsCaption: "Chọn...",
+		    requiredError: "Xin vui lòng trả lời câu hỏi.",
+		    requiredInAllRowsError: "Vui lòng trả lời các câu hỏi trong tất cả các hàng.",
+		    numericError: "Giá trị phải là số.",
+		    textMinLength: "Vui lòng nhập ít nhất {0} ký hiệu.",
+		    textMaxLength: "Vui lòng nhập ít hơn {0} ký hiệu.",
+		    textMinMaxLength: "Vui lòng nhập nhiều hơn {0} và ít hơn {1} ký hiệu.",
+		    minRowCountError: "Vui lòng điền vào ít nhất {0} hàng.",
+		    minSelectError: "Vui lòng chọn ít nhất {0} phương án.",
+		    maxSelectError: "Vui lòng không chọn nhiều hơn {0} phương án.",
+		    numericMinMax: "'{0}' phải bằng hoặc nhiều hơn {1} và bằng hoặc ít hơn {2}",
+		    numericMin: "'{0}' phải bằng hoặc nhiều hơn {1}",
+		    numericMax: "'{0}' phải bằng hoặc ít hơn {1}",
+		    invalidEmail: "Vui lòng nhập địa chỉ e-mail hợp lệ.",
+		    urlRequestError: "Yêu cầu đã trả về lỗi '{0}'. {1}",
+		    urlGetChoicesError: "Yêu cầu trả lại dữ liệu trống hoặc thuộc tính 'đường dẫn' không chính xác",
+		    exceedMaxSize: "Kích thước tệp không được vượt quá {0}.",
+		    otherRequiredError: "Vui lòng nhập giá trị khác.",
+		    uploadingFile: "Tệp của bạn đang tải lên. Vui lòng chờ vài giây và thử lại.",
+		    addRow: "Thêm dòng",
+		    removeRow: "Xóa dòng"
+		};
+surveyLocalization.locales["vi"] = vietnamSurveyStrings;
